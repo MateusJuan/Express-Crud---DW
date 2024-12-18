fb1ff53dba84fe08f9be52fd1d62c026bcc83593
aluno <169060996+MateusJuan@users.noreply.github.com>
2024-12-18 10:13:45 -0300

    18/12/2024

--- app.js
+++ app.js
@@ -6,16 +6,41 @@ const port = 3000;
 
 app.use (bodyParser.json());
 
-let pessoas = [
-    {id : 1, nome: 'Mateus', idade: 16},
-    {id : 2, nome: 'Eliel', idade: 16},
-    {id : 3, nome: 'João', idade: 35}
+let musicas = [
+    {id : 1, nome: 'Hino Nacional Brasileiro'},
+    {id : 2, nome: 'Cancão do xpedicionáirio'},
+    {id : 3, nome: 'Dobrado Baptista de Melo'}
 ];
 
-app.get('/pessoa',(res)=>{
-    res.json(pessoas);
+app.get('/musica',(res)=>{
+    res.json(musicas);
 });
 
-app.get('/pessoa/:id',(req,res)=>{
+app.get('/musica/:id',(req,res)=>{
     const id = parseInt(req.params.id);
+    const musica = musicas.find(musica => musica.id === id);
+
+    if (musica){
+        res.json(musica);
+    }else{
+        res.status(404).json({message: 'Musica não encontrada'});
+    }
+});
+
+app.post('/musica',(req,res)=>{
+    const {id,nome} = req.body;
+
+    if (!id || !nome{
+        res.status(400).json({message: 'Preencha todos os campos'});
+    }
+    
+    musica.push({id,nome});
+    res.status(201).json({message:'Musica Adiconada com sucesso',musica:{id,nome}});
+});
+
+app.put('/musica/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const {nome} = req.body;
+
+    const musica = musicas.find(musica => musica.id === id);
 });
